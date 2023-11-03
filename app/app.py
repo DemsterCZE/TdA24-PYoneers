ca2255303628b669b2f285209b49c0278f4a11c7
Demster <petr.zemanek1234@email.cz>
2023-11-03 23:03:09 +0100

    Udelano 60% ukolu

--- app/app.py
+++ app/app.py
@@ -1,10 +1,9 @@
 import os
 
-from flask import Flask
+from flask import Flask,render_template
 from . import db
 
 app = Flask(__name__)
-
 app.config.from_mapping(
     DATABASE=os.path.join(app.instance_path, 'tourdeflask.sqlite'),
 )
@@ -20,7 +19,7 @@ db.init_app(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return "Mužeme pracovat"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
