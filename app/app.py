46b0ac542267c3cf3730e0fd80523fd00768d93f
FilipRhapsody <jenkeeeboi@email.cz>
2023-12-31 20:31:32 +0100

    Zobrazení specifického lektora a začátek úpravy lektora

--- app/app.py
+++ app/app.py
@@ -43,8 +43,8 @@ def card():
 def preadd():
     return render_template("lecturercreate.html")
 
-@app.route('/api/lecturer/post', methods=["POST"])
-def post():
+@app.route('/api/lecturer', methods=["POST"])
+def addLecturer():
     if request.method=="POST":
         title_before=request.form.get("title_before")
         first_name=request.form.get("first_name")
@@ -66,21 +66,74 @@ def post():
         return render_template("index.html")
 
 
-@app.route('/api/lecturer/get')
+@app.route('/api/lecturer')
 def showAll():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM table1')
     profile = cur.fetchall()
+    print(profile)
     cur.close()
     return render_template("lecturershowall.html", profile=profile)
 
-@app.route('/api/lecturer/get/uuid')
-def showLecturer():
-    pass
+@app.route('/api/lecturer/<int:id>')
+def showLecturer(id):
+    cur =mysql.connection.cursor()
+    cur.execute("select * from table1 where id=(%s)",(id,))
+    profile = cur.fetchone()
+    print(profile[10])
+    cur.close()
+    return render_template("card.html", profile=profile)
 
-@app.route('/api/lecturer/put/uuid')
-def editLecturer():
-    pass
+@app.route('/api/lecturer/<int:id>/edit')
+def preEditLecturer(id):
+    return render_template("lectureredit.html", id=id)
+
+@app.route('/api/lecturer/<int:id>', methods=["POST"])
+def editLecturer(id):    
+    if request.method=="POST":
+        cur =mysql.connection.cursor()
+        print(id)
+        cur.execute("SELECT * FROM table1 WHERE id=(%s)",(id,))
+        profile = cur.fetchone()
+      
+        
+        cur.close()
+        id=str(request.form.get("id"))
+        titleBefore=str(request.form.get("title_before_edit"))
+        firstName=str(request.form.get("first_name_edit"))
+        middleName=str(request.form.get("middle_name"))
+        lastName=str(request.form.get("last_name"))
+        titleAfter=str(request.form.get("title_after"))
+        pictureUrl=str(request.form.get("picture_url"))
+        location=str(request.form.get("location"))
+        claim=str(request.form.get("claim"))
+        bio=str(request.form.get("bio"))
+        pricePerHour=str(request.form.get("price_per_hour"))
+        telephoneNumber=str(request.form.get("telephone_numbers"))
+        emails=str(request.form.get("emails"))
+        tagsNames=str(request.form.get("tags_names"))
+
+        print(f"předtitul:{titleBefore}")
+
+        profileEdit=[id,titleBefore,firstName,middleName,lastName,titleAfter,pictureUrl,location,claim,bio,pricePerHour,telephoneNumber,emails,tagsNames]
+
+        for index,value in enumerate(profileEdit):
+            
+            if value == "":
+                print(profileEdit[index],profile[index])
+                profileEdit[index]=profile[index]
+                
+        cur =mysql.connection.cursor()
+        cur.execute("UPDATE table1 SET title_before=%s, first_name=%s, middle_name=%s, last_name=%s, title_after=%s, picture_url=%s, location=%s, claim=%s, bio=%s, price_per_hour=%s, telephone_numbers=%s, emails=%s, tags_names=%s WHERE id=(%s)",(titleBefore, firstName, middleName, lastName, titleAfter, pictureUrl, location, claim, bio, pricePerHour, telephoneNumber, emails, tagsNames, id))
+        mysql.connection.commit()
+        cur.close()
+        cur =mysql.connection.cursor()
+        cur.execute("SELECT * FROM table1 WHERE id=(%s)",(id,))
+        profile = cur.fetchone()
+       
+        cur.close()
+        return render_template("card.html", profile=profile)
+    
 
 @app.route('/api/lecturer/delete/uuid')
 def deleteLecturer():
